4fdee425b3c0389a08079655f51e3270f02e421e
Omiiii <omghotekar01@gmail.com>
2026-09-11 17:21:48 +0530

    feat: export framework-specific TrustKernel guards

--- sdk/trustkernel/__init__.py
+++ sdk/trustkernel/__init__.py
@@ -1,4 +1,22 @@
 from .client import TrustKernelClient, TrustKernelDecision, GuardedExecution
 from .integrations import guard_callable, MCPGuard
+from .frameworks import (
+    FrameworkGuard,
+    protect_autogen_function,
+    protect_langchain_tool,
+    protect_langgraph_node,
+    protect_mcp_callable,
+)
 
-__all__ = ["TrustKernelClient", "TrustKernelDecision", "GuardedExecution", "guard_callable", "MCPGuard"]
+__all__ = [
+    "TrustKernelClient",
+    "TrustKernelDecision",
+    "GuardedExecution",
+    "guard_callable",
+    "MCPGuard",
+    "FrameworkGuard",
+    "protect_langchain_tool",
+    "protect_langgraph_node",
+    "protect_autogen_function",
+    "protect_mcp_callable",
+]
